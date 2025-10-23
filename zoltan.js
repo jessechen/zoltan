fa5fd1a6c2bd4eb1ecb0796c113f0a0862c2cb37
Jesse Chen <jesse.z.chen@gmail.com>
2025-10-23 13:46:41 -0400

    Successfully use encoder library

--- zoltan.js
+++ zoltan.js
@@ -1,1 +1,8 @@
-console.log('It works!');
+const encoder = new ReceiptPrinterEncoder();
+
+const result = encoder
+    .line('The is the first line')
+    .line('And this is the second')
+    .encode();
+
+console.log(result);
