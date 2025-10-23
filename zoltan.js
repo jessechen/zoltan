f6f7e590ae8414a630fc1578bb5630677c39aafb
Jesse Chen <jesse.z.chen@gmail.com>
2025-10-23 14:14:41 -0400

    First draft of auth

--- zoltan.js
+++ zoltan.js
@@ -1,21 +1,39 @@
 const encoder = new ReceiptPrinterEncoder();
 
-const raw = encoder
-    .initialize()
-    .align('center')
-    .line("today's fortune:")
-    .newline()
-    .size(2)
-    .line('NAND')
-    .size(1)
-    .line('no bogarting')
-    .newline()
-    .line('this stuff is for everyone.')
-    .line('take what you need')
-    .line('as long as you share.')
-    .newline()
-    .line('zoltan.recurse.com')
-    .cut()
-    .encode();
+function tokenFrom(cookie) {
+  return (
+    cookie
+      .split("; ")
+      .find((row) => row.startsWith("receipt_csrf="))
+      ?.split("=")[1] || ""
+  );
+}
 
-console.log(raw);
+function send() {
+    const raw = encoder
+        .initialize()
+        .align('center')
+        .line("today's fortune:")
+        .newline()
+        .size(2)
+        .line('NAND')
+        .size(1)
+        .line("don't bogart me")
+        .newline()
+        .line('this stuff is for everyone.')
+        .line('take what you need')
+        .line('as long as you share.')
+        .newline()
+        .line('zoltan.recurse.com')
+        .cut()
+        .encode();
+
+    console.log(raw);
+}
+
+const token = tokenFrom(document.cookie);
+if (token) {
+    document.getElementById('auth').classList.add('hidden');
+    document.getElementById('content').classList.remove('hidden');
+    document.getElementById('submit').addEventListener('click', send);
+}
